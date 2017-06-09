8572affd3d4565d42864279bf823656e64250429
Yato <memohamed1.dollars@gmail.com>
2017-06-09 18:14:38 +0400

    Changed how tags are handled

--- yara.py
+++ yara.py
@@ -1,7 +1,31 @@
 import discord
 from discord.ext import commands
+
 client = discord.Client()
 bot = commands.Bot(command_prefix='=')
+tags = []
+tagtext = []
+
+def ReadExistingTags():
+    with open('tags.txt', 'r') as f:
+        for line in f:
+            TagLine = line.split('|')
+            tags.append(TagLine[0])
+    return tags
+
+def ReadExistingTagText():
+    with open('tags.txt', 'r') as f:
+        for line in f:
+            TagLine = line.split('|')
+            tagtext.append(TagLine[1])
+    return tagtext
+
+
+def WriteNewTag(linetowrite):
+    WritingTag = open('tags.txt', 'a')
+    WritingTag.write(linetowrite)
+    WritingTag.close()
+# ------------------------------------------------------------------------------------------------------------------
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -9,44 +33,40 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+
 @client.event
 async def on_message(message):
-    if message.content.startswith('yara.tags'):
-        MessageString = ''
-        TagsList = open("tags list.txt", 'r')
-        StringMaker= TagsList.readline()
-        MessageString += StringMaker
-        await client.send_message(message.channel, str(MessageString))
-    if message.content.startswith('yara.tag'):
-        if message.content.startswith('yara.tag.new'):  # Creating a new tag
-            CommandName,CommandContent = message.content[len('yara.tag.new')+1:].split(" | ")            TagsList = open("tags list.txt", 'r')
-            ReadTagsList = TagsList.readline()
-            while len(ReadTagsList) > 0:
-                if CommandName in TagsList == True:
-                    TagFound = True
-                else: TagFound = False
-            if TagFound == False:
-                filehandle = open('tags.txt', 'a')
-                CommandString = '\n {0} {1}'.format(CommandName, CommandContent)
-                filehandle.write(CommandString)
-                messagesend = 'The tag `{0}` has been added'.format(CommandName)
-                TagsList = open("tags list.txt", 'a').write
-                TagsList('{0} \n'.format(CommandName))
-                await client.send_message(message.channel, messagesend)
-            else:
-                await client.send_message(message.channel, 'This tag already exists')
-        else:  # getting the tag
-            filehandle = open('tags.txt', 'r')
-            textline = filehandle.readline()
-            CommandFound = False
-            Command, CommandName = message.content.split(' ')
-            while textline.startswith(CommandName) == False and len(textline) > 0:
-                print(textline)
-                textline = filehandle.readline()
-                CommandFound = True
-            if CommandFound == True:
-                CommandMessage = textline[len(CommandName):]
-            else: CommandMessage = 'tag not found'
-            await client.send_message(message.channel, CommandMessage)
+    if message.content == '-tags':
+        ReadExistingTags()
+        messagestring = ' '
+        for i in tags:
+            messagestring = messagestring + i + ', '
+        await client.send_message(message.channel, 'available tags are `{0}`'.format(messagestring))
+        print(message.content, message.author.name, message.channel)
+    elif message.content.startswith('-createtag '):
+        linetowrite = message.content[len('-createtag '):] + '\n'
+        ReadExistingTags()
+        tagname = linetowrite.split('|')
+        if tagname[0] in tags:
+            await client.send_message(message.channel, 'tag already exists')
+        else:
+            WriteNewTag(linetowrite)
+            await client.send_message(message.channel, 'tag `{0}` created :+1:'.format(tagname[0]))
+        print(message.content)
+    elif message.content.startswith('-tag'):
+        tagname = message.content[len('-tag '):]
+        ReadExistingTags()
+        if tagname in tags:
+            count = 0
+            for i in tags:
+                count += 1
+                if tagname == i:
+                    break
+            ReadExistingTagText()
+            messagestring = tagtext[count-1]
+            await client.send_message(message.channel, messagestring)
+        else:
+            await client.send_message(message.channel, 'tag does not exist')
+
 #token = token
 #client.run(token)
